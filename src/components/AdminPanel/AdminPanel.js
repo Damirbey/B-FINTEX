0e42dde7292d346488936f30211fed3f4c92d872
Damirbey <dydamirbey@gmail.com>
2021-10-05 23:08:42 -0400

    Created the Search box in the Administrator page

--- src/components/AdminPanel/AdminPanel.js
+++ src/components/AdminPanel/AdminPanel.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './AdminPanel.css';
 import {Link} from 'react-router-dom';
 import { users, posts } from '../../data';
 
 const AdminPanel = ()=>{
+    
+    const [searchString, setSearchString] = useState('');
+
+    const onInputChange=(event)=>{
+        setSearchString(event.target.value);
+    }
+    const filteredUsers = users.filter((user)=>{
+        return user.name.toLowerCase().includes(searchString.toLowerCase()) || user.surname.toLowerCase().includes(searchString.toLowerCase())
+    })
+
     return(
        <div>
            <p><Link className="adminLink" to="/adminpanel">All Users</Link> / <Link className="adminLink" to="/allposts">Posts</Link> / </p>
-           
+           <input 
+            className="form-control mr-sm-2 " 
+            type="search" 
+            placeholder="Search Users by Name" 
+            aria-label="Search"
+            onChange={onInputChange}
+            />
+            <br/>
            <table class="table table-striped table-hover">
                 <thead>
                     <tr>
@@ -16,12 +33,11 @@ const AdminPanel = ()=>{
                     <th scope="col">Last Name</th>
                     <th scope="col">Email</th>
                     <th scope="col">Active</th>
-                    <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        users.map((user)=>{
+                        filteredUsers.map((user)=>{
                             return(
                             <tr key={user.id}>
                                 <th scope="row">{user.id}</th>
@@ -29,7 +45,6 @@ const AdminPanel = ()=>{
                                 <td>{user.surname}</td>
                                 <td>{user.email}</td>
                                 <td>{user.active}</td>
-                                <td><Link to={`/users/${user.id}`}><button type="button" class="btn btn-primary">Edit</button></Link> {user.id!==2 && <button type="button" class="btn btn-danger">Delete</button>}</td>
                             </tr>)
                         })
                     }
