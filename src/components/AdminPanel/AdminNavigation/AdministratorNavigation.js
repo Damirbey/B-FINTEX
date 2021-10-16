4ed97c7a74983bccc1e6c083610f881d0439c990
Damirbey <dydamirbey@gmail.com>
2021-10-16 14:30:39 -0400

    Working on the Post Details page

--- src/components/AdminPanel/AdminNavigation/AdministratorNavigation.js
+++ src/components/AdminPanel/AdminNavigation/AdministratorNavigation.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const AdministratorNavigation=()=>{
     return(
-        <p><Link className="adminLink" to="/adminpanel">All Users</Link> / <Link className="adminLink" to="/allposts">All Posts</Link> / </p>
+        <p><Link className="adminLink" to="/adminpanel">All Users</Link> / <Link className="adminLink" to="/allposts">All Posts</Link> / <Link className="adminLink" to="/allposts">Add New Post</Link> /</p>
     )
 }
 
