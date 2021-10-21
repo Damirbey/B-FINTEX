42470f1fa5d30b6cd73dae12edf3321a331e1af5
Damirbey <dydamirbey@gmail.com>
2021-10-20 23:32:39 -0400

    Completed the AddNewPost page for the admin

--- src/components/AdminPanel/Posts/AddNewPost/AddNewPost.js
+++ src/components/AdminPanel/Posts/AddNewPost/AddNewPost.js
@@ -9,26 +9,91 @@ const AddNewPost = ()=>{
     const [category, setCategory] = useState('');
     const [image, setImage] = useState('');
     const [content, setContent] = useState('');
+    const [successBox, setSuccessBox] = useState(false);
+    const [errorBox, setErrorBox] = useState(false)
+    const [displayMessage, setDisplayMessage] = useState('');
 
     /** Declaring onChange functions for every field on the form */
     const onTitleChange = (event)=>{
         setTitle(event.target.value);
+        resetField("#postTitle");
     }
 
     const onAuthorChange = (event)=>{
         setAuthor(event.target.value);
+        resetField("#author");
     }
 
     const onCategoryChange = (event)=>{
         setCategory(event.target.value);
+        resetField("#category");
     }
 
     const onImageChange = (event)=>{
         setImage(event.target.value);
+        resetField("#image")
     }
 
     const onContentChange=(event)=>{
         setContent(event.target.value);
+        resetField("#postContent");
+    }
+
+    /**Highlight and Reset functions are used to highlight the field when nothing 
+     * is entered and reset the field after the value is entered*/
+    const resetField=(id)=>{
+        document.querySelector(id).style.border="";
+    }
+    const highlightField=(id)=>{
+        document.querySelector(id).style.border="1px solid red";
+    }
+    const fieldName=(id)=>{
+        return document.querySelector(id).value;
+    }
+    /**A function that checks if any field is missing the user input  */
+    const allFieldsHaveInput=()=>{
+        var errorCount=0;
+        if(fieldName("#postTitle").length==0)
+        {
+            highlightField("#postTitle");
+            errorCount++;
+        }
+        if(fieldName("#author").length==0){
+            highlightField("#author");
+            errorCount++;
+        }
+        if(fieldName("#category").length==0){
+            highlightField("#category");
+            errorCount++;
+        }
+        if(fieldName("#image").length==0){
+            highlightField("#image");
+            errorCount++;
+        }
+        if(fieldName("#postContent").length==0){
+            highlightField("#postContent");
+            errorCount++;
+        }
+
+        if(errorCount>0)
+        {
+            return false;
+        }else{
+            return true;
+        }
+    }
+    /**Submitting the post */
+    const onSubmit=()=>{
+        if(allFieldsHaveInput())
+        {
+            setSuccessBox(true);
+            setErrorBox(false);
+            setDisplayMessage('Post added successfully');
+        }else{
+            setErrorBox(true);
+            setSuccessBox(false);
+            setDisplayMessage('Please fill in all the fields');
+        }
     }
 
     return(
@@ -44,7 +109,7 @@ const AddNewPost = ()=>{
                                     <h2 className="mb-2 text-primary">Add New Post</h2>
                                 </div>
                                 {
-                                    /*successBox ? 
+                                    successBox ? 
                                     <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 alert alert-success" role="alert">
                                         {displayMessage}
                                     </div>
@@ -52,7 +117,7 @@ const AddNewPost = ()=>{
                                     <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 alert alert-danger" role="alert">
                                         {displayMessage}
                                     </div>
-                                    : ""*/
+                                    : ""
                                 }
 
                                 <div className="col-xl-12 col-lg-12 col-md-12 col-sm-6 col-12">
@@ -100,7 +165,7 @@ const AddNewPost = ()=>{
                             <div className="row gutters">
                                 <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                                     <div className="text-right">
-                                        <button type="button" id="submit" name="submit" className="btn btn-primary" > Add Post </button>
+                                        <button type="button" id="submit" name="submit" className="btn btn-primary" onClick={onSubmit}> Add Post </button>
                                     </div>
                                 </div>
                             </div>
