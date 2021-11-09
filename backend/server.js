25dd7270bab9c55a519f73d7007615cdf9a23dfb
Damir <dyodgorov@fortresstechnology.com>
2021-11-08 22:47:47 -0500

    Implementing the backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Server is running, good job Damir");
+})
+
+app.listen(4000,function(){
+    console.log("Application is running on port 5000");
+})
