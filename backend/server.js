ef87e1403fbeb76f86b5d985752b7cb3564e975d
Damir <dyodgorov@fortresstechnology.com>
2021-11-08 22:51:36 -0500

    Implementing the backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Server is running, good job Damir");
+    res.send("Server is running");
 })
 
 app.listen(4000,function(){
