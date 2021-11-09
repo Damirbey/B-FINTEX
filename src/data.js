e95095e84409e859bfc5e3adbfe346bd18e0ffe5
Damir <dyodgorov@fortresstechnology.com>
2021-11-08 22:57:54 -0500

    testing git

--- src/data.js
+++ src/data.js
@@ -79,6 +79,15 @@ const posts = [
         publishedDate : "August 15,2021",
         category : "Side Post",
         image : "/images/news/adobe.png"
+    },
+    {
+        id:"9",
+        title: "Twitter asks users to flag COVID-19 and election misinformation",
+        content : "Choosing an insurance policy is one of the most complicated financial decisions a person can make. Jakarta-based Lifepal wants to simplify the process for Indonesians with a marketplace that lets users compare policies from more than 50 providers, get help from licensed agents and file claims. The startup, which says it is the country’s largest direct-to-consumer insurance marketplace, announced today it has raised a $9 million Series A. The round was led by ProBatus Capital, a venture firm backed by Prudential Financial, with participation from Cathay Innovation and returning investors Insignia Venture Partners, ATM Capital and Hustle Fund.",
+        author : "Lionel Messi",
+        publishedDate : "August 15,2021",
+        category : "Side Post",
+        image : "/images/news/adobe.png"
     }
 ]
 
