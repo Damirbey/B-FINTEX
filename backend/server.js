66b023fe214221b615666de8a2e4275e25999c3d
Damirbey <dydamirbey@gmail.com>
2021-11-10 22:46:44 -0500

    Building REST APIs

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,21 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
+/**Sign In request */
 app.post("/signIn", (req,res)=>{
     const {email, password} = req.body;
     console.log("Email is "+email);
     console.log("Password is "+password);
     res.json("Server is running");
+})
+/**Register request */
+app.post("/register",(req,res)=>{
+    const {email,name,surname,password} = req.body;
+    console.log("Register email "+email);
+    console.log("Register name "+name);
+    console.log("Register surname "+surname);
+    console.log("Register password "+password);
+    res.json("Registration is successful");
 })
 
 app.listen(PORT,function(){
