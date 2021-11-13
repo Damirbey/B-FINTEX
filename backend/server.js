fa6a9ea25693211e9032f935e02463ffb3003ec6
Damirbey <dydamirbey@gmail.com>
2021-11-12 23:34:01 -0500

    Working on the endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+/**Connecting to the database */
+const connectionString = {
+    host:'localhost',
+    port:5432,
+    database:'bfintex',
+    user:'postgres',
+    password:'123'
+}
+const db = pgp(connectionString);
 
 /**Sign In request */
 app.post("/signIn", (req,res)=>{
@@ -21,6 +32,7 @@ app.post("/register",(req,res)=>{
     console.log("Register name "+name);
     console.log("Register surname "+surname);
     console.log("Register password "+password);
+    db.any("INSERT INTO users (name,surname,email,hash,joined,active) VALUES ($1,$2,$3,$4,$5,$6)",[name,surname,email,password,new Date(),1]).then(response=>console.log(response)).catch(err=>console.log(err));
     res.json("Registration is successful");
 })
 
