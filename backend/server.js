5a099c9fea1f248f755f6ab849aa98ea9d6332af
Damirbey <dydamirbey@gmail.com>
2021-11-23 22:05:32 -0500

    Trying to store image in the postgre database

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,11 @@ const PORT = process.env.PORT || 3000;
 const signIn = require('./controllers/signIn');
 const register = require('./controllers/register');
 const users = require('./controllers/users');
+const fileUpload = require('express-fileupload');
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(fileUpload());
 
 /**Connecting to the database */
 const connectionString = {
@@ -45,11 +47,11 @@ app.delete("/deleteUser",users.deleteUser(db));
 
 /**Adding new post */
 app.post("/addNewPost",(req,res)=>{
-    const {postTitle,postAuthor,postImage,postContent} = req.body;
-    console.log("Image "+postImage)
-    db.any("INSERT INTO posts (post_title,author,post_content,image,date_added) VALUES ($1,$2,$3,$4,$5)",[postTitle,postAuthor,pg_read_binary_file(postImage),postContent,new Date()])
-    .then(response=>res.json("Success"))
-    .catch(err=>res.status(400).json("Error"));
+    const {postTitle,postAuthor,postContent} = req.body;
+ 
+    console.log(req)
+   /* db.any("INSERT INTO posts (post_title,author,post_content,image,date_added) VALUES ($1,$2,$3,$4,$5)",[postTitle,postAuthor,lo_import('"'+postImage+'"',68583),postContent,new Date()])
+    .then(response=>res.json("Success"))*/
 });
 
 app.listen(PORT,function(){
