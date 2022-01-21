9960b31e88e326da186b526ee5de41234690f202
Damirbey <dydamirbey@gmail.com>
2022-01-20 23:03:49 -0500

    Working on the backend

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.get("/img/:id",(req,res)=>{
     var id = req.params.id;
     db.any("SELECT image FROM posts WHERE post_id = $1",[id])
     .then(image=>{
-           res.send(<h1>Hello</h1>);
+           res.end(image.image);
         
     }).catch(err=>console.log(err));
 })
