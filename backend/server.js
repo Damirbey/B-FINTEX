8fe01e5505e5d41ea5a0b9d5ae2c88a4023d98c0
Damirbey <dydamirbey@gmail.com>
2022-02-22 22:09:58 -0500

    Testing

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.get("/img/:id",(req,res)=>{
     var id = req.params.id;
     db.any("SELECT image FROM posts WHERE post_id = $1",[id])
     .then(image=>{
-    res.send(image)
+    console.log(image)
     }).catch(err=>console.log(err));
 
 })
